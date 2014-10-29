e5cfc21b5f8267410244908e37e38d33898d7ea1
Cale Tilford <cale.tilford13@imperial.ac.uk>
2014-10-29 18:18:44 +0000

    Java class for outputting parse tree and bash script for running

--- src/wacc/slack/Compiler.java
+++ src/wacc/slack/Compiler.java
@@ -1,10 +1,30 @@
 package wacc.slack;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import antlr.WaccLexer;
+import antlr.WaccParser;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Compiler {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		String inputFile = null;
+		if (args.length > 0) {
+			inputFile = args[0];
+		}
+		InputStream is = System.in;
+		if (inputFile != null) {
+			is = new FileInputStream(inputFile);
+		}
+		ANTLRInputStream input = new ANTLRInputStream(is);
+		WaccLexer lexer = new WaccLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		WaccParser parser = new WaccParser(tokens);
+		ParseTree tree = parser.program();
+		System.out.println(tree.toStringTree(parser));
 	}
 
 }
