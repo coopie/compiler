cd5b90bae4f745469eb740c63e7128b8d2973c6b
Timotej Kapus <tk1713@corona22.doc.ic.ac.uk>
2014-10-31 17:46:49 +0000

    ast builder

---
+++ src/wacc/slack/AST/ASTBuilder.java
@@ -0,0 +1,198 @@
+package wacc.slack.AST;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import antlr.WaccParser.ArgListContext;
+import antlr.WaccParser.ArrayElemContext;
+import antlr.WaccParser.ArrayLiterContext;
+import antlr.WaccParser.ArrayTypeContext;
+import antlr.WaccParser.AssignLhsContext;
+import antlr.WaccParser.AssignRhsContext;
+import antlr.WaccParser.BaseTypeContext;
+import antlr.WaccParser.BinaryOperContext;
+import antlr.WaccParser.BoolLiterContext;
+import antlr.WaccParser.ExprContext;
+import antlr.WaccParser.FuncContext;
+import antlr.WaccParser.IntLiterContext;
+import antlr.WaccParser.IntSignContext;
+import antlr.WaccParser.PairElemContext;
+import antlr.WaccParser.PairElemTypeContext;
+import antlr.WaccParser.PairLiterContext;
+import antlr.WaccParser.PairTypeContext;
+import antlr.WaccParser.ParamContext;
+import antlr.WaccParser.ParamListContext;
+import antlr.WaccParser.ProgramContext;
+import antlr.WaccParser.StatContext;
+import antlr.WaccParser.TypeContext;
+import antlr.WaccParser.UnaryOperContext;
+import antlr.WaccParserVisitor;
+
+public class ASTBuilder implements WaccParserVisitor<WaccAST> {
+
+	@Override
+	public WaccAST visit(@NotNull ParseTree arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitChildren(@NotNull RuleNode arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitErrorNode(@NotNull ErrorNode arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitTerminal(@NotNull TerminalNode arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitIntSign(IntSignContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitAssignRhs(AssignRhsContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitArgList(ArgListContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitArrayType(ArrayTypeContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitParam(ParamContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitPairElem(PairElemContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitArrayElem(ArrayElemContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitBinaryOper(BinaryOperContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitParamList(ParamListContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitExpr(ExprContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitType(TypeContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitStat(StatContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitBoolLiter(BoolLiterContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitUnaryOper(UnaryOperContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitBaseType(BaseTypeContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitProgram(ProgramContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitPairType(PairTypeContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitArrayLiter(ArrayLiterContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitAssignLhs(AssignLhsContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitPairLiter(PairLiterContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitFunc(FuncContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitIntLiter(IntLiterContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WaccAST visitPairElemType(PairElemTypeContext ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/wacc/slack/AST/ProgramAST.java
+++ src/wacc/slack/AST/ProgramAST.java
@@ -1,10 +1,20 @@
 package wacc.slack.AST;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import antlr.WaccParser.FuncContext;
+import antlr.WaccParser.StatContext;
 
 public class ProgramAST implements WaccAST {
 	
-	List<FuncAST> func;
-	StatAST stat;
+	private final List<FuncAST> func = new LinkedList<>();
+//	private final StatAST stat;
+	
+	public ProgramAST(List<FuncContext> func2, StatContext stat2) {
+		
+		
+	}
+	
 
 }
