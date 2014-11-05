5650089c573508c653e85cfeea49b37f75ed86ac
Cale Tilford <cale.tilford13@imperial.ac.uk>
2014-11-05 13:24:56 +0000

    Removed ExprTest and returned Compiler.java to working form

--- src/wacc/slack/Compiler.java
+++ src/wacc/slack/Compiler.java
@@ -24,11 +24,7 @@ public class Compiler {
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		WaccParser parser = new WaccParser(tokens);
 		ParseTree tree = parser.program();
-		//System.out.println(tree.toStringTree(parser));
-		int errors = parser.getNumberOfSyntaxErrors();
-		if (errors > 0) {
-			System.exit(100);
-		}
+		System.out.println(tree.toStringTree(parser));
 	}
 
 }
