b1b6c5e405cc3af872ab620570af9dd15b584425
Cale Tilford <cale.tilford13@imperial.ac.uk>
2014-11-11 15:42:51 +0000

    ExitStatementAST returns error if expr is non-int

--- src/wacc/slack/AST/statements/ExitStatementAST.java
+++ src/wacc/slack/AST/statements/ExitStatementAST.java
@@ -2,11 +2,18 @@ package wacc.slack.AST.statements;
 
 import wacc.slack.FilePosition;
 import wacc.slack.AST.Expr.ExprAST;
+import wacc.slack.AST.types.BaseType;
+import wacc.slack.errorHandling.errorRecords.ErrorRecords;
+import wacc.slack.errorHandling.errorRecords.IllegalOperationError;
 
 public class ExitStatementAST extends ExprStatementAST {
 
 	public ExitStatementAST(ExprAST exprAST, FilePosition filePos) {
 		super(exprAST, filePos);
+		if (exprAST.getType() != BaseType.T_int) {
+			ErrorRecords.getInstance().record(
+					new IllegalOperationError(filePos));
+		}
 	}
 
 	@Override
