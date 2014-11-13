f336cf1815427f648947d8dcd61fdf1b5cd82ba6
Sam Coope <sam.j.coope@gmail.com>
2014-11-13 14:50:56 +0000

    fixed bug in getType of fstAST

--- src/wacc/slack/AST/types/PairType.java
+++ src/wacc/slack/AST/types/PairType.java
@@ -28,18 +28,24 @@ public class PairType implements Type {
 		return filePos;
 	}
 	
-	public boolean equals(Type t) {
+	@Override
+	public boolean equals(Object o) {
 		
 		// if we are checking that a pair has the same type as null:
-		if ((this.snd == null && this.snd ==  null))
-				{
-			return t instanceof PairType;
-		} else {
-			return t instanceof PairType &&
-					fst.equals(((PairType) t).fst) &&
-					snd.equals(((PairType) t).snd);
+		if (o instanceof PairType) {
+			PairType pt = (PairType)o;
+			// the type of null is (null,null), this is checking that we are
+			// comparing to a null value
+			
+			if((this.fst    == null && this.snd    == null)
+			 ||(pt.getFst() == null && pt.getSnd() == null)) {
+				return true;
+			}
 		}
 
+		return o instanceof PairType &&
+				fst.equals(((PairType) o).fst) &&
+				snd.equals(((PairType) o).snd);
 	}
 	
 	public Type getFst() {
