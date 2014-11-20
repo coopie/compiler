cb88cd341690362d0897fa741d4626db32cef0ef
Samuel Coope <sjc113@ic.ac.uk>
2014-11-20 13:50:50 +0000

    Added contion enum to reduce copied classes

--- src/wacc/slack/instructions/GeneralArmInstruction.java
+++ src/wacc/slack/instructions/GeneralArmInstruction.java
@@ -7,9 +7,17 @@ abstract class GeneralArmInstruction {
 	protected final Operand source;
 	protected final Operand dest;
 	
+	protected Condition cond = Condition.AL;
+	
 	public GeneralArmInstruction(Operand dest, Operand source) {
 		this.source = source;
 		this.dest = dest;
+	}
+	
+	public GeneralArmInstruction(Operand dest, Operand source, Condition cond) {
+		this.source = source;
+		this.dest = dest;
+		this.cond = cond;
 	}
 	
 	public Operand getSource() {
