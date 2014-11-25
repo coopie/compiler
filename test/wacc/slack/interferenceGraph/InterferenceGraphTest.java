f82b922a9f4f4d9a4ae1bcf1dd3efa6d0235ce9d
Sam Coope <sam.j.coope@gmail.com>
2014-11-25 17:54:26 +0000

    added framework and tdd made InterferenceGraphNode

---
+++ test/wacc/slack/interferenceGraph/InterferenceGraphTest.java
@@ -0,0 +1,32 @@
+package wacc.slack.interferenceGraph;
+
+import org.junit.Test;
+
+public class InterferenceGraphTest {
+
+	@Test
+	public void canIterateThroughAllNodes() {
+		
+	}
+	
+	@Test
+	public void canCleanItself() {
+		
+	}
+	
+	@Test
+	public void isNotConstrainedWhenNodeHasLessThanKNeighbours() {
+		
+	}
+	
+	@Test
+	public void isConstrainedWhenNodeHasMoreThanKNeighbours() {
+		
+	}
+	
+	@Test
+	public void canGetNeighbours() {
+		
+	}
+
+}
